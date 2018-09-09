dcfa26ba7c98413c6085bec2250b05f93f67ba78
shane <shane@development.development>
2018-09-09 17:27:01 -0400

    closer to solving decoding problem stopped using deocding method from 3rd party

--- qtUi/RadioProcessing.py
+++ qtUi/RadioProcessing.py
@@ -1,15 +1,18 @@
 from rtlsdr import RtlSdr
 import numpy as np
 import scipy.signal as signal
+import scipy.io
+from scipy.io import wavfile
 import matplotlib
 import time
 import pyaudio
 import sys
 import os
-import wave
+
 import threading
 from threading import Thread
-matplotlib.use('Agg') # necessary for headless mode
+from contextlib import closing
+#matplotlib.use('Agg') # necessary for headless mode
 # see http://stackoverflow.com/a/3054314/3524528
 
 
@@ -17,90 +20,45 @@ class populateRadioData(object):
 
     def grabRaw(self, F_station):
 
-        sdr = RtlSdr()
-        F_offset = 250000         # Offset to capture at
-        # We capture at an offset to avoid DC spike
-        Fc = F_station - F_offset # Capture center frequency
-        Fs = int(1140000)         # Sample rate
-        N = int(4147200)          # Samples to capture
+        with closing(RtlSdr()) as sdr:  
+            sdr.sample_rate = sample_rate = 240000
+            sample_rate_fm = 240000
+            sdr.center_freq = 101.1e6
+            sdr.gain = 20
+            iq_samples = sdr.read_samples(4976640)
 
-        # configure device
-        sdr.sample_rate = Fs      # Hz
-        sdr.center_freq = Fc      # Hz
-        sdr.gain = 'auto'
+            iq_comercial = signal.decimate(iq_samples, sample_rate//sample_rate_fm)
 
-        # Read samples
-        samples = sdr.read_samples(N)
+            angle_comercial = np.unwrap(np.angle(iq_comercial))
+            demodulated_comercial = np.diff(angle_comercial)
 
-        # Clean up the SDR device
-        #sdr.close()
-        #del(sdr)
+            audio_rate = 48000
+            audio_comercial = signal.decimate(demodulated_comercial, \
+            sample_rate_fm//audio_rate, zero_phase=True)
 
-        # Convert samples to a numpy array
-        x1 = np.array(samples).astype("complex64")
+            audio_comercial = np.int16(1e4*audio_comercial)
+            wavfile.write("comercial_demodulated.wav", rate=audio_rate, data=audio_comercial)
 
-        # To mix the data down, generate a digital complex exponential
-        # (with the same length as x1) with phase -F_offset/Fs
-        fc1 = np.exp(-1.0j*2.0*np.pi* F_offset/Fs*np.arange(len(x1)))
-        # Now, just multiply x1 and the digital complex expontential
-        x2 = x1 * fc1
-
-        # An FM broadcast signal has a bandwidth of 200 kHz
-        f_bw = 200000
-        n_taps = 64
-
-        # Use Remez algorithm to design filter coefficients
-        lpf = signal.remez(n_taps, [0, f_bw, f_bw+(Fs/2-f_bw)/4, Fs/2], [1,0], Hz=Fs)
-        x3 = signal.lfilter(lpf, 1.0, x2)
-
-        dec_rate = int(Fs / f_bw)
-        x4 = x3[0::dec_rate]
-        # Calculate the new sampling rate
-        Fs_y = Fs/dec_rate
-
-        # Polar discriminator
-        y5 = x4[1:] * np.conj(x4[:-1])
-        x5 = np.angle(y5)
-
-        # The de-emphasis filter
-        # Given a signal 'x5' (in a numpy array) with sampling rate Fs_y
-        d = Fs_y * 75e-6   # Calculate the # of samples to hit the -3dB point
-        x = np.exp(-1/d)   # Calculate the decay between each sample
-        b = [1-x]          # Create the filter coefficients
-        a = [1,-x]
-        x6 = signal.lfilter(b,a,x5)
-
-        # Find a decimation rate to achieve audio sampling rate between 44-48 kHz
-        audio_freq = 44100.0
-        dec_audio = int(Fs_y/audio_freq)
-        Fs_audio = Fs_y / dec_audio
-
-        x7 = signal.decimate(x6, dec_audio)
-
-        # Scale audio to adjust volume
-        x7 *= 10000 / np.max(np.abs(x7))
-        # Save to file as 16-bit signed single-channel audio samples
-
-        save = x7.astype("int16").tofile("wbfm-mono1.raw")
-
-        print(Fs_audio)
  
-        os.system("sox -r 45600 -e unsigned -b 16 -c 1 wbfm-mono1.raw wbfm1.wav")
-            
-
     def playBack(self):
 
-        chunk = 1048576 
+        #startup = 1
+        #if startup == 1:
+        #    time.sleep(7)
+        #    startup += 1
+
+        import wave
+        chunk = 1024
         #open a wav format music  
-        f = wave.open("wbfm1.wav","rb")
+        f = wave.open("comercial_demodulated.wav","rb")
         #instantiate PyAudio  
         p = pyaudio.PyAudio()  
         #open stream  
         stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-                       channels = f.getnchannels(),  
-                       rate = f.getframerate(),  
+                        channels = f.getnchannels(),  
+                        rate = f.getframerate(),  
                         output = True)
- 
+     
         #read data  
         data = f.readframes(chunk)  
 
@@ -108,16 +66,15 @@ class populateRadioData(object):
         while data:  
             stream.write(data)      
             data = f.readframes(chunk)
-            
-        #stop stream  
-        #stream.stop_stream()  
-        #stream.close()  
+                
+            #stop stream  
+            #stream.stop_stream()  
+            #stream.close()  
 
-        #close PyAudio  
-        #p.terminate()
-
-
-
+            #close PyAudio  
+            #p.terminate()
+        
+    
 if __name__ == "__main__":
                     
     
